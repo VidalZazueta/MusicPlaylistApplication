1616878aa910bb1a28cc468c56f346057a28cdd0
Vidal Zazueta <vzazuet26@gmail.com>
2025-10-10 21:12:14 -0700

    Initializing project structure with all folders and files necessary to submit. Added code for the app file and server runs on localhost

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'dotenv/config';
+
+import playlists from './api/routes/playlists.js';
+import tracks from './api/routes/tracks.js';
+import users from './api/routes/users.js';
+
+const PORT = 8888;
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/playlists', playlists);
+app.use('/tracks', tracks);
+app.use('/users', users);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
