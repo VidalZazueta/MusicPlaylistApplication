b56a51eb604f025dd12a230a310adca1bf7f8efa
Vidal Zazueta <vzazuet26@gmail.com>
2026-01-21 22:26:40 -0800

    Added the ability to delete a playlist

--- frontend/src/components/PlaylistCard/PlaylistCard.jsx
+++ frontend/src/components/PlaylistCard/PlaylistCard.jsx
@@ -2,10 +2,16 @@
  * @description Render one playlist with no data fetching
  * @returns A list item - playlist
  */
-function PlaylistCard ({ playlist }) {
+function PlaylistCard ({ playlist, onDelete }) {
     return (
-        <li>
-            {playlist.title}
+        <li style={{ display: "flex", justifyContent: "space-between" }}>
+            <span>{playlist.title}</span>
+
+            <button 
+            onClick={() => onDelete(playlist._id)}
+            style= {{ marginLeft: "1rem"}}>
+                Delete
+            </button>
         </li>
     )
 }
